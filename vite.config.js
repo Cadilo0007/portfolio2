a42538f351eef85e7c28b652cb73376ea14f8d88
Justine Leymark Cadillo <152825592+Cadilo0007@users.noreply.github.com>
2025-04-12 22:53:57 +0800

    Update vite.config.js
    initial commit

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: "/portfolio2",
+  base: "/portfolio2/",
   assetsInclude: ['**/*.glb'],
 })
